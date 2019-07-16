448f44e54d91165ebd2ff835f28c22c434c9f010
Dustin Jefford <djefford58@gmail.com>
2019-07-16 08:36:08 -0400

    Added local files

---
+++ NodeJS/my_parser.js
@@ -0,0 +1,22 @@
+#!javascript
+// Require my new parser.js file
+var Parser = require('./parser');
+
+// Load the fs (filesystem) module
+var fs = require('fs');
+
+// Read the contents of the file into memory.
+fs.readFile('example_log.txt', function (err, logData) {
+
+  // If an error occurred, throwing it will
+  // display the exception and end our app.
+  if (err) throw err;
+
+  // logData is a Buffer, convert to string.
+  var text = logData.toString();
+
+  var parser = new Parser();
+
+  console.log(parser.parse(text));
+  // { A: 2, B: 14, C: 6 }
+})
---
+++ PowerShell/test1.ps1
@@ -0,0 +1,13 @@
+# Testing various PowerShell commands
+# Get-Process
+
+# Get-Process -Name "Google Chrome"
+
+# Get-Process -Name 'Google Chrome', 'App Store'
+# Clear-Host
+
+# New-Item -Path ./test.txt -Value "Hello World!" -Force
+# Set-Content -Path ./test.txt -Value "Hello World Again!"
+# Get-Content -Path ./test.txt
+# Remove-Item -Path ./test.txt
+
