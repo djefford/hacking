6d3b92179b7adf57a9b0730171a0c7f9d70d05f1
Dustin Jefford <djefford58@gmail.com>
2019-07-17 15:51:24 -0400

    Added new route

--- Python/flask/app.py
+++ Python/flask/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__) #app is the name of the object here
 def home():
   return "<h1> Hello World </h1>"
 
+@app.route('/hello')
+def hello():
+  return "<h3> This is hello 2 </h3>"
+
 if __name__ =="__main__":
   app.run(debug=True,port=8080)
