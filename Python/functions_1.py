edac17519fa16930d346b24f4faf2614c7652f12
Dustin Jefford <djefford58@gmail.com>
2019-07-17 08:33:34 -0400

    Initialized files

---
+++ Python/functions_1.py
@@ -0,0 +1,13 @@
+# Declaring a function.
+def greet():
+  print("Hello ! Welcome to the party.")
+
+greet()
+
+def greet_name(name):
+    print("Hello {}".format(name))
+
+
+greet_name("Flash")
+greet_name("Arrow")
+greet_name("Bat")
