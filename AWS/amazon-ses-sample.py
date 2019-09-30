e8aa3a59fa6fa7c2820c1db4f925a4bf376f7c6a
Dustin Jefford <djefford58@gmail.com>
2019-09-30 11:01:57 -0400

    Updated with test body HTML

--- AWS/amazon-ses-sample.py
+++ AWS/amazon-ses-sample.py
@@ -14,17 +14,35 @@ BODY_TEXT = ("Amazon SES Test (Python)\r\n"
              "AWS SDK for Python (Boto)."
             )
 
-BODY_HTML = """<html>
-<head></head>
-<body>
-  <h1>Amazon SES Test (SDK for Python)</h1>
-  <p>This email was sent with
-    <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
-    <a href='https://aws.amazon.com/sdk-for-python/'>
-      AWS SDK for Python (Boto)</a>.</p>
-</body>
-</html>
-            """
+# BODY_HTML = """<html>
+# <head></head>
+# <body>
+#   <h1>Amazon SES Test (SDK for Python)</h1>
+#   <p>This email was sent with
+#     <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+#     <a href='https://aws.amazon.com/sdk-for-python/'>
+#       AWS SDK for Python (Boto)</a>.</p>
+# </body>
+# </html>
+#             """
+
+BODY_HTML = """
+<h1>Landing Zone Account Notification<br>
+---------------------------------------------</h1>
+<h3>Account: ######</h3>
+<h3>Region: ######</h3>
+<p>The following resources in the account are not tagged properly. Please log into the Landing Zone account and take corrective action on the resources listed below.<br>
+For a complete list, you can find Compliance results in the AWS Config Console (please ensure you are logged into the account specified above).</p>
+<p><a href='https://aws.amazon.com/ses/'>Amazon SES</a></p>
+
+<h4>Resource List
+<br>-----------------</h4>
+<p>ResourceID: </p>
+<p>ResourceID: </p>
+<h4>-----------------</h4>
+<p>For questions about this communication, please submit an email to the Landing Zone team:<br>
+<a href="mailto:fakeList@fake.com">FakeList@fake.com</a></p>
+"""
 
 CHARSET = "UTF-8"
 
